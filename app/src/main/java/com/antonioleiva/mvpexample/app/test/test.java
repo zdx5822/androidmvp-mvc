817845aea09baeff761f7ec996b56e65635c42d8
zm5822 <648468670@qq.com>
2018-03-23 10:14:16 +0800

    添加一个方法

--- app/src/main/java/com/antonioleiva/mvpexample/app/test/test.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/test/test.java
@@ -1,8 +1,13 @@
 package com.antonioleiva.mvpexample.app.test;
 
+import android.util.Log;
+
 /**
  * Created by Administrator on 2018/3/23.
  */
 
 public class test {
+    public void test(){
+        Log.i( "test: ","system.out.println");
+    }
 }
